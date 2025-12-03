1e635181afc4691bdd20d10c7ca117413e9b6a25
Ilkin Bayramov <ilkinbayramov@Ilkin-MacBook-Pro.local>
2025-12-03 16:05:02 +0400

    Add custom SSL context for MongoDB Atlas connection

--- src/main/kotlin/com/ilkinbairamov/ninjatalk/database/MongoDatabase.kt
+++ src/main/kotlin/com/ilkinbairamov/ninjatalk/database/MongoDatabase.kt
@@ -3,7 +3,12 @@ package com.ilkinbairamov.ninjatalk.database
 import com.ilkinbairamov.ninjatalk.models.User
 import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
+import java.security.SecureRandom
+import java.security.cert.X509Certificate
 import java.util.concurrent.TimeUnit
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 import org.litote.kmongo.coroutine.coroutine
 import org.litote.kmongo.reactivestreams.KMongo
 
@@ -14,17 +19,39 @@ object MongoDatabase {
         connectionString = uri
     }
 
+    private fun createTrustAllSslContext(): SSLContext {
+        val trustAllCerts =
+                arrayOf<TrustManager>(
+                        object : X509TrustManager {
+                            override fun checkClientTrusted(
+                                    chain: Array<X509Certificate>,
+                                    authType: String
+                            ) {}
+                            override fun checkServerTrusted(
+                                    chain: Array<X509Certificate>,
+                                    authType: String
+                            ) {}
+                            override fun getAcceptedIssuers(): Array<X509Certificate> = arrayOf()
+                        }
+                )
+
+        val sslContext = SSLContext.getInstance("TLS")
+        sslContext.init(null, trustAllCerts, SecureRandom())
+        return sslContext
+    }
+
     private val client by lazy {
         val settings =
                 MongoClientSettings.builder()
                         .applyConnectionString(ConnectionString(connectionString))
                         .applyToSocketSettings { builder ->
-                            builder.connectTimeout(10, TimeUnit.SECONDS)
-                            builder.readTimeout(10, TimeUnit.SECONDS)
+                            builder.connectTimeout(30, TimeUnit.SECONDS)
+                            builder.readTimeout(30, TimeUnit.SECONDS)
                         }
                         .applyToSslSettings { builder ->
                             builder.enabled(true)
                             builder.invalidHostNameAllowed(true)
+                            builder.context(createTrustAllSslContext())
                         }
                         .build()
 
